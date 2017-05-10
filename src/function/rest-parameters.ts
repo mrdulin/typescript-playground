b4dfe6f17b087cc6322a7bb516171baf9d12d099
mrdulin <novaline@qq.com>
2017-05-10 11:52:56 +0800

    add two function examples

---
+++ src/function/rest-parameters.ts
@@ -0,0 +1,20 @@
+function add(...args: number[]): number{
+  let result = 0;
+  for(let arg of args) {
+    result += arg;
+  }
+  return result;
+}
+
+const results = [
+  add(),
+  add(2),
+  add(2,2),
+  add(2,2,2),
+  add(2,2,2,2),
+  add(2,2,2,2,2)
+];
+
+for(let result of results) {
+  console.log(result);
+}
