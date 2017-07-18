680a8d20c3d013d6f500b662d7fe4bb311a8d60a
mrdulin <novaline@qq.com>
2017-07-18 15:18:05 +0800

    update

--- src/type/typing-destructured-object-literal-and-aray.ts
+++ src/type/typing-destructured-object-literal-and-aray.ts
@@ -1,8 +1,8 @@
 /**
  * Created by dulin on 2017/6/27.
  */
-namespace typingDestructuredObjectLiteralAndArray{
-  
+namespace typingDestructuredObjectLiteralAndArray {
+
   const action = {
     type: 'SEARCH/QUERY_BOOK',
     payload: {
@@ -13,35 +13,38 @@ namespace typingDestructuredObjectLiteralAndArray{
     meta: {
       query: 'react',
       page: 1
-    } 
+    }
   };
 
-  // -- 对象字面量结构赋值 --
+  // -- 对象字面量解构赋值 --
 
-  const {total, books}: {total: number, books: Array<string>} = action.payload;
+  const { total, books }: { total: number, books: string[] } = action.payload;
   console.log(total, books);
-  
-  
+
+  // -- 对象字面量解构赋值 + rest operator --
+  const { x, y, ...rest }: { x: number, y: number, [key: string]: number } = { x: 1, y: 2, z: 3, q: 4 };
+  console.log(rest);
+
   // -- 数组结构赋值 - 1 --
-  const [firstBook, secondBook, lastBook]: Array<string> = action.payload.books;
+  const [firstBook, secondBook, lastBook]: string[] = action.payload.books;
   console.log(firstBook, secondBook, lastBook);
 
   // -- 数组结构赋值 - 2 --
   const names: [string, string, string] = ['react', 'angular', 'typescript'];
-  const [q,w,e]: [string, string, string] = names;
-  console.log(q,w,e);
+  const [q, w, e]: [string, string, string] = names;
+  console.log(q, w, e);
 
   // -- 数组结构赋值 - 3 --
   const [a, b]: [number, number] = [1, 1];
   console.log(a, b);
-  
-  
+
+
   // -- 下面这两种方式都不行 --
   // 因为books被类型推断成了Array<string>类型
   // const [firstBook, secondBook, lastBook]: [string, string, string] = action.payload.books;
   // const [firstBook, ...rest]: [string, string[]] = action.payload.books;
-  
-  
+
+
 }
 
 
