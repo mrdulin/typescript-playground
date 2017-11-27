fcd4f72e15db8b19bea1a754c4d4bcd9e8bf4b20
mrdulin <novaline@qq.com>
2017-11-27 14:00:09 +0800

    添加请求超时的例子

--- src/async-await/demo-3.ts
+++ src/async-await/demo-3.ts
@@ -1,11 +1,45 @@
-const A = () => new Promise((resolve) => setTimeout(resolve, 1000, '123'));
+// async/await的请求超时
 
-const obj = {
-  async main() {
-    const value = await A();
-    console.log('value', value);
-    return value;
-  }
+const timeout = () => new Promise((resolve, reject) => {
+  setTimeout(() => {
+    console.log('timeout timestamp: ', Date.now());
+    reject('请求超时~');
+  }, 1000);
+});
+
+const coin = () => Math.random() < 0.5;
+const request = () => new Promise((resolve, reject) => {
+  console.log('request timestamp: ', Date.now());
+  setTimeout(() => {
+    const coinValue = coin();
+    const result = coinValue ? '请求成功' : '请求失败';
+    coinValue ? resolve(result) : reject(result);
+  }, 1100);
+});
+
+//使用promise的方式
+const main = () => {
+  Promise.race([timeout(), request()])
+    .then((result) => {
+      console.log('result', result);
+    })
+    .catch((e) => {
+      console.error(e);
+    });
 };
 
-console.log(obj.main());
+// main();
+
+//使用async/await的方式
+async function componentDidMount() {
+  let result, error;
+  try {
+    result = await Promise.race([request(), timeout()]);
+  } catch (e) {
+    error = e;
+  }
+  console.log(result, error);
+}
+
+componentDidMount();
+
