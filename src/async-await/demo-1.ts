f36733d635c984f93079dd056d707cfadd737445
mrdulin <novaline@qq.com>
2017-11-27 16:28:26 +0800

    修改请求超时的例子

--- src/async-await/demo-1.ts
+++ src/async-await/demo-1.ts
@@ -1,29 +1,35 @@
-/**
- * 现有A,B,C三个接口，相互之间没有依赖关系，
- */
+// async/await + try...catch
 
-class Component {
-  public label: string = 'ts';
-  public m(x = this.label) {
-    console.log(x);
-  }
+const coin = () => Math.random() < 0.5;
+const request = () => new Promise((resolve, reject) => {
+  console.log('request timestamp', Date.now());
+  setTimeout(() => {
+    const coinValue = coin();
+    const result = coinValue ? '请求成功' : '请求失败';
+    coinValue ? resolve(result) : reject(result);
+  }, 1000);
+});
+const timeout = () => new Promise((resolve, reject) => {
+  console.log('timeout timestamp', Date.now());
+  setTimeout(reject, 900, new Error('请求超时'));
+});
 
-  public a = (x = this.label) => {
-    console.log(x);
+async function main() {
+  try {
+    return await Promise.race([
+      timeout(),
+      request()
+    ])
+      .then((result) => {
+        console.log('result', result);
+        return Promise.reject('请求依旧失败');
+      });
+  } catch (e) {
+    console.log('catch', e);
+    return e;
   }
 }
-const comp = new Component();
-comp.m();
-comp.a();
 
-
-function main(x: any = (function (ctx: any) { console.log(ctx); })(global as any)) {
-  console.log('main');
-}
-
-let a = null;
-let b;
-// main(a);
-// main(b);
+main();
 
 export { };
