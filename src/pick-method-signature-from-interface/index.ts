8646446e4b71950133b5d76b9caa0da5dc2b23ea
mrdulin <novaline.dulin@gmail.com>
2020-02-04 14:56:25 +0800

    add sample

---
+++ src/pick-method-signature-from-interface/index.ts
@@ -0,0 +1,17 @@
+interface IRequestOptions {
+  body?: any;
+  params: any;
+}
+interface IRequest {
+  someProperty: string;
+  post(uri: string, options: IRequestOptions): any;
+  get(uri: string, options?: IRequestOptions): any;
+}
+
+type PickMethod<T, MethodName extends keyof T> = T[MethodName] extends (
+  ...args: any[]
+) => any
+  ? (...args: Parameters<T[MethodName]>) => ReturnType<T[MethodName]>
+  : never;
+
+type PostMethodType = PickMethod<IRequest, "post">;
