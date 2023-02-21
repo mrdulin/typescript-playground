ed7c656883dd957735fece7ffdf7cf4a3b7a062f
mrdulin <novaline.dulin@gmail.com>
2023-02-21 19:22:13 +0800

    feat: add generic types

--- http-request/02/index.ts
+++ http-request/02/index.ts
@@ -1,7 +1,8 @@
 import * as t from 'io-ts';
 import * as TE from 'fp-ts/lib/TaskEither';
+import * as T from 'fp-ts/lib/Task';
 import * as E from 'fp-ts/lib/Either';
-import { pipe } from 'fp-ts/lib/function';
+import { flow, pipe } from 'fp-ts/lib/function';
 import { failure } from 'io-ts/lib/PathReporter';
 
 export interface ApiResponse<Result = any> {
@@ -17,11 +18,22 @@ const GetArticlesByPageRequestDTOCodec = t.type({
 
 type GetArticlesByPageRequestDTO = t.TypeOf<typeof GetArticlesByPageRequestDTOCodec>;
 
+const ArticleDTOCodec = t.type({
+  id: t.number,
+  title: t.string,
+});
+type ArticleDTO = t.TypeOf<typeof ArticleDTOCodec>;
+
 type GetArticlesByPageQueryPayload = {
   pagination: any;
 };
 
-const getArticlesByPage = async (data: GetArticlesByPageRequestDTO) => {
+type PaginationResult<T> = {
+  resultList: T[];
+  totalItem: number;
+};
+
+const getArticlesByPage = async (data: GetArticlesByPageRequestDTO): Promise<ApiResponse<PaginationResult<ArticleDTO>>> => {
   return {
     code: '0',
     message: '',
@@ -36,11 +48,11 @@ const getArticlesByPage = async (data: GetArticlesByPageRequestDTO) => {
 };
 
 const getArticlesByPageTaskEither = (data: GetArticlesByPageRequestDTO) =>
-  TE.tryCatch<Error, ApiResponse>(
+  TE.tryCatch<Error, ApiResponse<PaginationResult<ArticleDTO>>>(
     () =>
       getArticlesByPage(data).then((res) => {
         if (res.code !== '0') {
-          throw new Error(res.message);
+          throw new Error(res.message || 'biz error');
         }
         return res;
       }),
@@ -68,3 +80,42 @@ export const getArticlesByPageService = ({ pagination }: GetArticlesByPageQueryP
     ),
   );
 };
+
+const PaginationResultCodec = <C extends t.Mixed>(codec: C) =>
+  t.type({
+    resultList: codec,
+    totalItem: t.number,
+  });
+
+const ApiResponseCodec = <C extends t.Mixed>(codec: C) =>
+  t.type({
+    code: t.string,
+    message: t.union([t.string, t.undefined]),
+    result: codec,
+  });
+
+const GetArticlesByPageResponseCodec = ApiResponseCodec(PaginationResultCodec(t.array(ArticleDTOCodec)));
+
+export const decodeApiResponse = (res: ApiResponse<PaginationResult<ArticleDTO>>) => {
+  return pipe(
+    E.of(res),
+    E.chain(flow(ArticleDTOCodec.decode, E.mapLeft(E.toError))),
+    E.fold(
+      (e) => 'no',
+      (res) => 'yes',
+    ),
+  );
+};
+
+const r = decodeApiResponse({
+  code: '0',
+  message: '',
+  result: {
+    resultList: [
+      { id: 1, title: 'test' },
+      { id: 2, title: 'test1' },
+    ],
+    totalItem: 100,
+  },
+});
+console.log('r: ', r);
